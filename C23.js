bfbb863473a6a333ebebc0cc8f055f8ab4d75e56
Bamzy123 <stephenomotos@gmail.com>
2025-01-29 09:55:23 +0100

    my work

---
+++ C23.js
@@ -0,0 +1,9 @@
+
+
+let a = 1;
+let x = a + 10;
+let y = a - 10;
+let z = a / 10;
+
+let result = (a/(x*y*z))
+console.log(Math.round(result))
---
+++ test.js
@@ -0,0 +1,3 @@
+let js = "amazing";
+
+if (js === "amazing") console.log("JavaScript is fun")
