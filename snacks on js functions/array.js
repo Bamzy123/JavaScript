f4bba71fcbc9c7f1cc46e107c61579cb521d9c81
Bamzy123 <stephenomotos@gmail.com>
2025-02-05 07:44:01 +0100

    array work shop

--- snacks on js functions/array.js
+++ snacks on js functions/array.js
@@ -1,15 +1,27 @@
 const array = []; //LITERAL MRTHOD
 
 const arrayOfNumbers = new Array(); //ARRAY CONSTRUCTOR
-arrayOfNumbers.push(1);
-arrayOfNumbers.push(2);
-arrayOfNumbers.push(3);
+arrayOfNumbers[0] = 20;
+arrayOfNumbers[1] = 5;
+arrayOfNumbers.unshift(1)
+arrayOfNumbers.unshift(2);
+arrayOfNumbers.unshift(3);
 
-console.log(arrayOfNumbers);
+let shift = arrayOfNumbers.shift();
+// console.log(shift);
+// console.log(arrayOfNumbers);
 
 
-let firstName = "stephen"; //ARRAYOF
+// let firstName = "stephen"; //ARRAYOF
 
-let newArray = Array.from(firstName);
+// let newArray = Array.from(firstName);
 
-console.log(newArray);
+// console.log(newArray);
+
+
+let animals = ["dog", "chick", "goat", "fish", "ship"];
+
+// let answer = animals.splice();
+console.log(animals.slice());
+// console.log(animals);
+// console.log(animals.slice(1,3));
